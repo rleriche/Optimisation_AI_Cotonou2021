687536be992ba656670af0b8bf0f326d5431b191
Rodolphe Le Riche <Rodolphe.LE_RICHE@uca.fr>
2021-07-19 16:01:08 +0200

    start gradient

--- Project/3Dplots.R
+++ Project/3Dplots.R
@@ -1,7 +1,7 @@
 # Example file to show 2D functions in 3D plots
 # Assume here that dim = 2 (dim is the number of variables)
 # R. Le Riche
-rm(list=ls())
+rm(list=ls()) # clear environment
 
 library("rgl") # library for plots
 source("./test_functions.R")
@@ -11,19 +11,20 @@ dim <- 2
 # choose a function from test_functions.R file
 fun <- quadratic
 # upper and lower bounds
-LB<--5
-UB<-5
+LB<-c(-5,5) 
+UB<-c(5,5)
 
 # start drawing the function (necessarily dim=2)
 no.grid <- 300
-x <- seq(LB, UB, ,no.grid)
-x.grid <- expand.grid(x, x)
+x1 <- seq(LB[1], UB[1], length.out=no.grid)
+x2 <- seq(LB[2], UB[2], length.out=no.grid)
+x.grid <- expand.grid(x1, x2)
 z <- apply(x.grid, 1, fun)
 z.grid <- matrix(z, no.grid)
 
 #### 3D rgl plot
 open3d()
-surface3d(x, x, z.grid, col= "lightblue")
+surface3d(x1, x2, z.grid, col= "lightblue")
 title3d("quadratic", col="blue", font=4)
 decorate3d()
 aspect3d(1, 1, 1)
@@ -32,6 +33,6 @@ rgl.snapshot("./fileofplot.png", fmt="png", top=T)
 
 ### 2D contour plot
 # png(filename="./contour.png") # save the contour in the current directory
- contour(x, x, z.grid, nlevels=20, xlab="x", ylab="y")
+ contour(x1, x2, z.grid, nlevels=20, xlab="x1", ylab="x2")
 # dev.off()
 
--- Project/gradient_descent.R
+++ Project/gradient_descent.R
@@ -1,4 +1,49 @@
+#############################################
+# a constant step-size gradient algorithm
+# Rodolphe Le Riche
+#
+# Implementation notes: 
+# * no attempt at making this code efficient, it is for teaching purpose
+#############################################
 source('test_functions.R')
 source('utilities_optim.R')
 
-# work in progress
+### problem definition
+# search space
+d<-2 # dimension
+LB<-c(-5,-5) #lower bounds
+UB<-c(5,5) #upper bounds
+fun<-quadratic #function to minimize
+
+### algorithm settings
+xinit <- c(3,2) # initial point
+stepFactor <- 0.8 # constant step factor
+# the maximum number of calls to the function is larger because of the 
+# finite differences scheme
+printlevel <- 2 # controls how much is stored and printed
+#                 =1 store best
+#                 =2 store all points
+stopBudget <- 100 # maximum number of iterations
+stopGradNorm <- 1.e-8 # stop when mean gradient norm is smaller than 
+
+### initializations
+x <- xinit
+Fbest <- fun(x)
+iter <- 1
+recXbest <- matrix(x,nrow=1)
+recFbest <- Fbest
+recTime <- c(iter)
+if (printlevel >= 2){
+  recX <- matrix(x,nrow=1)
+  recF <- Fbest
+}
+gradf <- ffinite.diff(x=x,f=fun,h=1.e-8)
+
+### run the algo
+while ((iter <= stopBudget) | ((norm(as.matrix(df),type = "F")/sqrt(d)) > stopGradNorm) ){
+  
+  xstep <- 
+  
+}
+
+
