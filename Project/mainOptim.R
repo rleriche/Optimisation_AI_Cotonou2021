1d11b3281b857df332eb6b7d651ccee8cfe73531
Rodolphe Le Riche <leriche@emse.fr>
2021-07-25 16:30:01 +0200

    some gradient descent slides

--- Project/mainOptim.R
+++ Project/mainOptim.R
@@ -15,8 +15,8 @@ source('restarted_descent.R')
 ### problem definition
 # search space
 pbFormulation <- list()
-pbFormulation$fun<-schwefel #function to minimize, examples in test_functions.R
-d<-2
+pbFormulation$fun<-quadratic #function to minimize, examples in test_functions.R
+d<-10
 pbFormulation$d<-d # dimension
 pbFormulation$LB<-rep(-5,d) #lower bounds
 pbFormulation$UB<-rep(5,d) #upper bounds
@@ -24,23 +24,23 @@ pbFormulation$UB<-rep(5,d) #upper bounds
 
 ### algorithm settings
 optAlgoParam <- list()
-optAlgoParam$xinit <- c(4.5,3.5)#c(4.5,3.5) rep(-4.9,d) # initial point
+optAlgoParam$xinit <- rep(-4.,d)#c(4.5,3.5) rep(-4.9,d) # initial point
 #
-optAlgoParam$budget <- 3000
+optAlgoParam$budget <- 100
 optAlgoParam$minGradNorm <- 1.e-6 
 optAlgoParam$minStepSize <- 1.e-11 
 #
-optAlgoParam$direction_type <- "momentum" # choices are : "gradient", "momentum", "NAG"
-optAlgoParam$linesearch_type <- "armijo" # choices are: "none", "armijo"
+optAlgoParam$direction_type <- "gradient" # choices are : "gradient", "momentum", "NAG"
+optAlgoParam$linesearch_type <- "none" # choices are: "none", "armijo"
 optAlgoParam$stepFactor <- 0.1 # step factor when there is no line search, 
 optAlgoParam$beta <- 0.9 # momentum term for direction_type == "momentum" or "NAG" 
 # 
 printlevel <- 4 # controls how much is stored and printed, choices: 0 to 4
 
 # a single descent
-# res<-gradient_descent(pbFormulation=pbFormulation,algoParam=optAlgoParam,printlevel=printlevel)
+res<-gradient_descent(pbFormulation=pbFormulation,algoParam=optAlgoParam,printlevel=printlevel)
 
 # a restarted descent
-optAlgoParam$nb_restarts <- 5
-cres <- restarted_descent(pbFormulation=pbFormulation,algoParam = optAlgoParam,printlevel=printlevel)
+# optAlgoParam$nb_restarts <- 5
+# cres <- restarted_descent(pbFormulation=pbFormulation,algoParam = optAlgoParam,printlevel=printlevel)
   
